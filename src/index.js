4a3a71d8bc0ecbfb13c88995ee979375ec262632
Mohamed Sabry <mohamedsabry3023@gmail.com>
2022-08-01 21:16:00 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.scss';
+function component() {
+  const element = document.createElement('div');
+  
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
