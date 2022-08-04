3869e1e684452c47b365052ab2f2bf2f4cd61cbd
wadaudzai <wadaudzai@gmail.com>
2022-08-04 23:50:17 +0430

    Add new changes to page because of styling

--- src/modules/displayList.js
+++ src/modules/displayList.js
@@ -27,19 +27,46 @@ async function getLoc() {
   itemsCounter.textContent = totalItems(chapters);
 
   const mainSection = document.querySelector('.main-section');
+
   for (let i = 0; i < chapters.length; i += 1) {
+
     const div = document.createElement('div');
+    div.classList.add('card')
     mainSection.append(div);
+
     const imageDiv = document.createElement('div');
     imageDiv.classList.add('image-quran');
     div.append(imageDiv);
-    const title = document.createElement('h2');
+
+    const title = document.createElement('p');
+    title.classList.add('heading');
     div.id = chapters[i].id;
     div.append(title);
     title.textContent = `${chapters[i].name_simple}`;
-    const arabicName = document.createElement('h3');
+
+    const arabicName = document.createElement('p');
+    arabicName.classList.add('heading');
     div.append(arabicName);
     arabicName.textContent = `${chapters[i].name_arabic}`;
+
+    const likeComment = document.createElement('div');
+    likeComment.classList.add('likeComment-holder');
+    div.append(likeComment);
+
+    const resButton = document.createElement('button');
+    resButton.setAttribute('type', 'submit');
+    resButton.textContent = 'Comments';
+    resButton.addEventListener('click', commentsPopup);
+    likeComment.append(resButton);
+
+    const likeButton = document.createElement('input');
+    likeButton.type = 'checkbox';
+    likeButton.addEventListener('change', () => {
+      sendLike(chapters[i].id);
+      likeCount.textContent = parseInt(likeCount.textContent, 10) + 1;
+    });
+    likeComment.append(likeButton);
+
     const likeCount = document.createElement('p');
     likeOnApi.forEach((item) => {
       if (item.item_id === chapters[i].id) {
@@ -47,19 +74,8 @@ async function getLoc() {
       }
     });
     likeCount.textContent = likeCount.textContent || 0;
-    div.append(likeCount);
-    const likeButton = document.createElement('input');
-    likeButton.type = 'checkbox';
-    likeButton.addEventListener('change', () => {
-      sendLike(chapters[i].id);
-      likeCount.textContent = parseInt(likeCount.textContent, 10) + 1;
-    });
-    div.append(likeButton);
-    const resButton = document.createElement('button');
-    resButton.setAttribute('type', 'submit');
-    resButton.textContent = 'Comments';
-    resButton.addEventListener('click', commentsPopup);
-    div.append(resButton);
+    likeComment.append(likeCount);
+
   }
 }
 
